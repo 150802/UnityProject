db54be04bf354317566e0be7e69cfc5c703d3716
Matei <150802@student.horizoncollege.nl>
2020-10-26 22:55:08 +0100

    Initial upload
    Initial upload of the allready existing files, some things may be broken since i lost the original files in a hard drive problem but still had the files backed up in onedrive. Most things should work just some textures not appearing correctly.

---
+++ New Unity Project/Assets/Scripts/WeaponController.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+public class WeaponController : MonoBehaviour
+{
+    public int gunDamage = 1;
+    public float fireRate = .25f;
+    public float weaponRange = 50f;
+    public float hitForce = 50f;
+    public Transform cameraLeader;
+    public Transform gunEnd;
+    public Camera fpsCam;
+    public GameObject explotionPrefab;
+
+    private WaitForSeconds shotDuration = new WaitForSeconds(.07f);
+    private LineRenderer gunEffect;
+    private float nextFire;
+
+    private void Start()
+    {
+        gunEffect = GetComponent<LineRenderer>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetButtonDown ("Fire1") && Time.time > nextFire)
+        {
+            nextFire = Time.time + fireRate;
+
+            StartCoroutine(ShotEffect());
+
+            Vector3 rayOrigin = fpsCam.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, 0));
+            RaycastHit hit;
+
+            gunEffect.SetPosition(0, gunEnd.position);
+
+            if (Physics.Raycast(rayOrigin,fpsCam.transform.forward, out hit, weaponRange))
+            {
+                gunEffect.SetPosition(1, hit.point);
+                if (hit.rigidbody != null)
+                {
+                    hit.rigidbody.AddForce(hit.normal * hitForce);
+                }
+
+                
+
+                if (hit.collider.GetComponent<HealthScript>() != null) {
+                    hit.collider.GetComponent<HealthScript>().hit = true;
+                }
+            }
+            else
+            {
+                gunEffect.SetPosition(1, rayOrigin + (fpsCam.transform.forward * weaponRange));
+            }
+        }
+
+        if (Input.GetKeyDown("1") && Time.time > nextFire)
+        {
+            Vector3 rayOrigin = fpsCam.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, 0));
+            RaycastHit hit;
+
+            nextFire = Time.time + fireRate;
+
+            if (Physics.Raycast(rayOrigin, fpsCam.transform.forward, out hit, weaponRange))
+            {
+                gunEffect.SetPosition(1, hit.point);
+                Instantiate(explotionPrefab, new Vector3(hit.point.x, hit.point.y, hit.point.z), Quaternion.identity);
+            }
+            else
+            {
+                gunEffect.SetPosition(1, rayOrigin + (fpsCam.transform.forward * weaponRange));
+            }
+        }
+    }
+
+    private IEnumerator ShotEffect()
+    {
+        gunEffect.enabled = true;
+        yield return shotDuration;
+        gunEffect.enabled = false;
+    }
+}
