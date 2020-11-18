0b24e09083b2bbe80525eef55074116ddb7a0b96
150802 <150802@student.horizoncollege.nl>
2020-11-18 22:47:25 +0100

    changed the project version AGAIN
    its back to universal render pipeline and on the 2019 version since the 2020 version is broken and i also started working on some AI

---
+++ testproject/Assets/Scripts/BasicAIScript.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicAIScript : MonoBehaviour
+{
+    private GameObject envroiment;
+    private GameObject player;
+    private bool hasTarget = false;
+
+    private void Start()
+    {
+        player = GameObject.Find("Player");
+        envroiment = GameObject.Find("Envroiment");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        List<GameObject> extCoverLocations = envroiment.GetComponent<AIMindHive>().coverLocations;
+
+        if (extCoverLocations.Count != 0)
+        {
+            int chosenCover = Random.Range(0, extCoverLocations.Count);
+
+            while (hasTarget != true)
+            {
+                if (envroiment.GetComponent<AIMindHive>().coverLocations[chosenCover].GetComponent<CoverCounter>().CoverCount <= 5)
+                {
+                    envroiment.GetComponent<AIMindHive>().coverLocations[chosenCover].GetComponent<CoverCounter>().CoverCount++;
+                    this.gameObject.GetComponent<UnityEngine.AI.NavMeshAgent>().SetDestination(envroiment.GetComponent<AIMindHive>().coverLocations[chosenCover].transform.position);
+                    hasTarget = true;
+                }
+                else
+                {
+                    if(chosenCover < extCoverLocations.Count)
+                    {
+                        chosenCover++;
+                    } else {
+                        this.gameObject.GetComponent<UnityEngine.AI.NavMeshAgent>().SetDestination(player.transform.position);
+                        hasTarget = true;
+                    }
+                }
+                print(chosenCover);
+            }
+        }
+
+        print(extCoverLocations.Count);
+    }
+}
---
+++ testproject/Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    public GameObject myPrefab;
+    public int enemyCount;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for(int i = 0; i <= enemyCount; i++)
+        {
+            Instantiate(myPrefab, this.gameObject.transform.position, Quaternion.identity);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
